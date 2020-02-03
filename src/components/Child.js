0497d2001ab7068ba1fd582b6ba00b9cc98faa07
chefboyrdeuce <jpldeuce@gmail.com>
2020-02-02 20:57:17 -0500

    created ChildPage, Child, and ChildCard

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,1 +1,44 @@
 // child's name and age
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Child = (props) => {
+const [child, setchild] = useState();
+
+useEffect(() => {
+const id = props.match.params.id;
+
+
+    axios
+    .get(`http://localhost:5000/api/childs/${id}`)
+    .then(response => {
+        setchild(response.data);
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+},[]);
+
+if (!child) {
+return <div>Loading child information...</div>;
+}
+
+const { name, age } = child;
+return (
+<div className="save-wrapper">
+    <div className="child-card">
+        <div className="child-name">
+            Name: <em>{name}</em>
+        </div>
+        <div className="child-age">
+            Age: <strong>{age}</strong>
+        </div>
+    </div>
+</div>
+);
+}
+
+export default Child;
