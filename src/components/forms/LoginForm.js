0a452050191a209b295f501a5c43bc74b4d5c4bb
JC8747 <jonathan.chiaverini@gmail.com>
2020-02-02 19:29:32 -0500

    Login and Registry form structures created.

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,1 +1,17 @@
-// username and a password + submit button
+import React, {useState} from 'react';
+// import styled from 'styled-components';
+import { useForm } from "react-hook-form";
+
+
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(async (data) => await fetchAPI(data))}>
+      <input type="text" placeholder="Username" name="Username" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="text" placeholder="Password" name="Password" ref={register} />
+      <input type="submit" />
+    </form>
+  );
+};
--- src/components/forms/RegistryForm.js
+++ src/components/forms/RegistryForm.js
@@ -1,1 +1,18 @@
-// username, password + register button
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data, e) => {
+    console.log('Submit event', e)
+    alert(JSON.stringify(data))
+  };
+
+  return (
+    <form onSubmit={handleSubmit(async (data) => await fetchAPI(data))}>
+      <input name="UserName" ref={register} />
+      <input name="Password" ref={register} />
+      <button type="submit">Submit</button>
+    </form>
+  )
+};
