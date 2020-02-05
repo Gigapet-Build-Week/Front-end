6a92b827f22cd72b73367750a2c27302112ea0d2
chefboyrdeuce <jpldeuce@gmail.com>
2020-02-04 20:14:23 -0500

    added Router and react-router-dom to Index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import{ BrowserRouter as Router } from 'react-router-dom';
 
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
 <Provider store={store}>
-    <App />
+    <Router>
+        <App />
+    </Router>
 </Provider>,
 document.getElementById('root')
 );
