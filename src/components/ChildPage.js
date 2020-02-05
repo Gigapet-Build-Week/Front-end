64f0f15a4e77f1b2e43b9160ba1c510d8e304d53
Trey McGarity <trey.mcgarity99@gmail.com>
2020-02-05 02:13:23 -0600

    childpage refractored, possible Link update required

--- src/components/ChildPage.js
+++ src/components/ChildPage.js
@@ -1,15 +1,30 @@
 // map the list of Child cards
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { getChild, addChild } from '../redux/actions/userActions';
+import { connect } from 'react-redux';
 
-export default class ChildPage extends React.Component {
+class ChildPage extends React.Component {
     state = {
         newChild: {
             name: '',
             age: ''
         }
+    };
+
+    childDetails({ child }) {
+    const { name, age } = child;
+    return (
+    <div className="child-card">
+        <div className="child-name">
+        Name: <em>{name}</em>
+        </div>
+        <div className="child-age">
+        Age: <strong>{age}</strong>
+        </div>
+    </div>
+    )
     }
 
     addNewChild = event => {
@@ -26,19 +41,10 @@ export default class ChildPage extends React.Component {
         });
       };
 
-    render(){
+    render() {
         return (
-        // <div className="child-list">
-        //     {children.map(child => (
-        //     <Link to={`/children/${child.id}`}>
-        //     <childDetails key={child.id} child={child} />
-        //     </Link>
-        //     ))}
-        // </div>
-        // );
-        // }
         <div>
-        <form onSumbit={this.addNewChild}>
+            <form onSumbit={this.addNewChild}>
                 <input
                     type="text"
                     name="name"
@@ -53,24 +59,23 @@ export default class ChildPage extends React.Component {
                     onChange={this.changeHandler}
                     placeholder="age"
                 />
-                />
-                <button onClick={this.addNewChild}>add child</button>
-                </form>
-
+                <button onClick={this.addNewChild}>Add Child</button>
+            </form>
+            <div className="child-list">
+                {this.props.children.map(child => (
+                <Link to={`/children/${child.id}`}> {/*probably different url*/}
+                <childDetails key={child.id} child={child} />
+                </Link>
+                ))}
+            </div>
         </div>
         )
     }
 }
 
-// childDetails({ child }) {
-// const { name, age } = child;
-// return (
-// <div className="child-card">
-//     <div className="child-name">
-//     Name: <em>{name}</em>
-//     </div>
-//     <div className="child-age">
-//     Age: <strong>{age}</strong>
-//     </div>
-// </div>
-// );
+const mapStateToProps = state => ({
+    error: state.error,
+    loggingIn: state.loggingIn
+  });
+  
+export default connect( mapStateToProps, { getChild, addChild })(ChildPage);
